f13500f2964d4418dc75f3aa11ae4d280b208780
Justin Schellenberg <jrschellenberg@gmail.com>
2018-03-01 19:44:15 -0600

    added unrevisioned files

---
+++ models/loans.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Loans = sequelize.define('Loans', {
+    id: {
+	    type: DataTypes.INTEGER,
+	    primaryKey: true,
+	    autoIncrement:true
+    },
+    book_id: DataTypes.INTEGER,
+    patron_id: DataTypes.INTEGER,
+    loaned_on: DataTypes.DATE,
+    return_by: DataTypes.DATE,
+    returned_on: DataTypes.DATE,
+    underscored:true
+  }, {});
+  Loans.associate = function(models) {
+    Loans.belongsTo(models.Patrons);
+    Loans.belongsTo(models.Books);
+    // associations can be defined here
+  };
+  return Loans;
+};
