069658c5ace7da1812570c68513e1076e5c847cd
Justin Schellenberg <jrschellenberg@gmail.com>
2018-03-04 12:41:25 -0600

    built out controller/view for return_book

--- routes/books.js
+++ routes/books.js
@@ -1,34 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const Books = require("../models").Books;
 const Patrons = require("../models").Patrons;
+const dateFormat = require('dateformat');
 
-console.log(Books);
 
 /* GET users listing. */
 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
 	res.render('test', { title: 'Express' });
 });
 
-router.get('/new', function(req, res) {
-	//res.send("WTF?");
+router.get('/new', (req, res) => {
 	res.render('new_book', { title: 'Express' });
 });
 
-router.get('/all', function(req, res) {
-	//res.send("wtf?");
+router.get('/all', (req, res) => {
 	Books.findAll().then((books) => {
 		res.render('all_books', { title: 'Express', books: books });
 	});
 });
 
-router.get('/:id', function(req, res) {
+router.get('/return_book/:bookId/:patronName', (req, res) => {
+	Books.findById(req.params.bookId).then((book) => {
+		book.getLoans().then((loan) => {
+			let now = new Date();
+			let date = dateFormat(now, "yyyy-mm-dd");
+			res.render('return_book', { title: 'Return '+book.title, book: book, date: date,
+				name: req.params.patronName, loan: loan[0] });
+		});
+	});
+});
+
+//May have to come back to this. Only grabbing history for 1 patron atm..
+router.get('/:id', (req, res) => {
 	Books.findById(req.params.id).then((book) => {
 		book.getLoans().then((loans) => {
-			console.log(loans);
 			Patrons.findById(loans[0].dataValues.patron_id).then((patron) => {
-				//console.log(patron.first_name);
 				if(patron){ //Error handling for no patron name.
 					return patron.first_name + ' ' + patron.last_name;
 				}
@@ -43,7 +51,5 @@ router.get('/:id', function(req, res) {
 		});
 	});
 });
-
-
 
 module.exports = router;
